0e263cbb9e903d3b5932c6c2cc8c4711487b55a1
Nur Javier Prasetyo <nurjavierprasetyo8789@gmail.com>
2026-02-15 10:48:51 +0000

    Tugas Statistika

--- Another subject/statistika_dummy_data.java
+++ Another subject/statistika_dummy_data.java
@@ -1,5 +1,14 @@
 public class statistika_dummy_data {
     public static void main(String[] args) {
-        System.out.println("Awoks");
+        System.out.println("\nNilai rata-rata semester Budi\n");
+        System.out.println("|----------|-------|");
+        System.out.println("| Semester | Nilai |");
+        System.out.println("|----------|-------|");
+        System.out.println("|    1     | 83,25 |");
+        System.out.println("|    2     | 86,33 |");
+        System.out.println("|    3     | 90,63 |");
+        System.out.println("|    4     | 88,18 |");
+        System.out.println("|    5     | 87,80 |");
+        System.out.println("|----------|-------|");
     }
 }
