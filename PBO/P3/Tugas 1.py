45e3013e7e062d289e376c946d2fa9d32f83bc53
Nur Javier Prasetyo <nurjavierprasetyo8789@gmail.com>
2026-02-22 18:35:31 +0000

    Tugas PBO games

--- PBO/P3/Tugas 1.py
+++ PBO/P3/Tugas 1.py
@@ -37,41 +37,42 @@ class Grid:
         return
       self.board[object.y][object.x]=object.symbol
 
-    def move_object(self, oldObject, newX, newY):
-      if not self.check_bounds(oldObject.x, oldObject.y):
+    def move_object(self, charPosition, newX, newY):
+      if not self.check_bounds(charPosition.x, charPosition.y):
         print("Out of bounds!")
         return
       if not self.check_bounds(newX, newY):
         print("Out of bounds!")
         return
-      self.board[oldObject.y][oldObject.x] = "-"
+      self.board[charPosition.y][charPosition.x] = "-"
+      charPosition.x = newX
+      charPosition.y = newY
+      self.board[charPosition.y][charPosition.x] = charPosition.symbol
 
-      self.board[newY][newX]=oldObject.symbol
-
-# Here!
-
+# Init
 grid = Grid(10,10)
 print("Current grid size: " + str(grid.xs) + "x" + str(grid.ys) + "\nWith the anchor on top-left\n")
 
+# Init char
 Liz = GameObject("L", 1, 4)
 print("Initial Liz position: " + str(Liz.x + 1) + ", " + str(Liz.y + 1))
 grid.place_object(Liz)
 grid.display()
 print("======================")
 
+# Change position
 print("Changing position into: " + str(4 + 1) + ", " + str(5 + 1))
 
 print("======================")
-grid.move_object(Liz, Liz.x, Liz.y)
-Liz.x = 4
-Liz.y = 5
+grid.move_object(Liz, 4, 5)
 print("Current Liz position: " + str(Liz.x + 1) + ", " + str(Liz.y + 1))
 grid.display()
-# print("======================")
+print("======================")
 
-# print("Changing position into: " + str(10 + 1) + ", " + str(5 + 1))
+# Change position but outside grid
+print("Changing position into: " + str(10 + 1) + ", " + str(5 + 1))
 
-# print("======================")
-# grid.move_object(Liz, GameObject(Liz.symbol, 10, 5))
-# print("Current Liz position: " + str(Liz.x + 1) + ", " + str(Liz.y + 1))
-# grid.display()
+print("======================")
+grid.move_object(Liz, 10, 5)
+print("Current Liz position: " + str(Liz.x + 1) + ", " + str(Liz.y + 1))
+grid.display()
